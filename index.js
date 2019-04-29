1a34a9f20c50acee9db3a0b474a851f9f92e56a4
Shinnosuke Iwaide <iwaide@hoobby.net>
2019-04-29 13:22:46 +0900

    Improved Component

--- index.js
+++ index.js
@@ -5,12 +5,18 @@
 //   }
 // });
 Vue.component('todo-item', {
-  template: '<li>This is a todo</li>'
+  props: ['todo'],
+  template: '<li>{{todo.text}}</li>'
 })
 var app = new Vue({ 
   el: '#app',
   data: {
-    message: 'hogehoge'
+    message: 'hogehoge',
+    gloceryList: [
+      { id: 1, text: 'hoge' },
+      { id: 2, text: 'fuga' },
+      { id: 3, text: 'moge' },
+    ]
   },
   methods: {
     reverseMessage: function() {
